1989c48112c6f863f6645566a78bc7a8883d0a25
SanketLV <sanketlakhani80@gmail.com>
2026-03-27 10:24:15 +0530

    My First File

---
+++ 04module.py
@@ -0,0 +1,18 @@
+import math
+import calendar
+
+# print(math.sqrt(25))
+# print(math.pow(2, 4))
+# print(math.pi)
+# print(math.floor(2.3))
+# print(math.ceil(2.3))
+# print(math.log10(100))
+# print(math.log10(1000))
+
+cal = calendar.month(2026, 12)
+print(cal)
+
+# print(dir(calendar))
+print(calendar.isleap(2026))
+print(calendar.isleap(2016))
+print(calendar.isleap(2020))
